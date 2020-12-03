42b0e658ba0a1f5578970e8ad4d483247a9ac7a4
Joe Bradley <jlbradley1844@gmail.com>
2020-12-02 21:15:36 -0500

    This week's updates

--- _special-events/vibrant_communities.mk
+++ _special-events/vibrant_communities.mk
@@ -1,12 +1,20 @@
 ---
 layout: post
 title:  "Interfaith Devotional & Talk Series"
-description: "Three Strategies for Racial Unity. Join us for special guest Dr. June Manning Thomas."
+description: "The Role of Crisis in Humanity's Progress"
 location: Electronic
-time: Saturday, october 24, 2020, 7:30 PM
+time: Saturday, Dec. 19, 2020, 7:30 PM
 color: BF360C
+img: vibrant-communities-19-Dec-20.jpg
 ---
-Next in our interfaith devotional and talk series "Creating
-Vibrant communities" is Dr. June Manning Thomas, author and
-Professor Emeritus. See <https://westchestercvc.splashthat.com> to
+Next in our interfaith devotional and talk series is "The Role of
+Crisis in Humanity's Progress." The various challenges humanity
+faces can be understood as symptoms of deeper crises; what lessons
+must humanity learn in order to overcome these challenges?
+The presenter is Daniel Perell, a representative for the Baha'i
+International Community in the United Nations whos work includes
+social and sustainable development, gender equality, the environment,
+and other areas of concern.
+
+See <http://www.westchester-cvc.org/talk-series.html> to
 register. Contact <westchester.cvc@gmail.com> for more information.
