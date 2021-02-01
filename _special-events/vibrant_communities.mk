6167eee234b822c167319915e9093f1070a4bc7a
Joe Bradley <jlbradley1844@users.noreply.github.com>
2021-02-01 06:56:19 -0500

    Updates 2021 jan 6 (#21)
    * big batch o' January updates
    
    * Updates for 1/25/21
    
    * Update to add Feb 21 event
    
    * fix url

--- _special-events/vibrant_communities.mk
+++ _special-events/vibrant_communities.mk
@@ -3,7 +3,7 @@ layout: post
 title:  "Interfaith Devotional & Talk Series"
 description: "Interfaith Talk Series - Toward a Just Society: Reimagining Human Nature"
 location: "Electronic (Zoom)"
+exturl: http://www.westchester-cvc.org/talk-series.html
 time: Monthly. Next event Tuesday Feb. 21 4:30PM
 color: BF360C
-exturl: http://www.westchester-cvc.org/talk-series.html
 ---
