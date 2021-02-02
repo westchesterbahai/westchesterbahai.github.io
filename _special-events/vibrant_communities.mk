8eced34bdc5bb0f5253360265ff549feb424e1dd
Joe Bradley <jlbradley1844@gmail.com>
2021-02-02 06:32:39 -0500

    repair forwarding... again

--- _special-events/vibrant_communities.mk
+++ _special-events/vibrant_communities.mk
@@ -4,6 +4,7 @@ title:  "Interfaith Devotional & Talk Series"
 description: "Interfaith Talk Series - Toward a Just Society: Reimagining Human Nature"
 location: "Electronic (Zoom)"
 exturl: http://www.westchester-cvc.org/talk-series.html
-time: Monthly. Next event Tuesday Feb. 21 4:30PM
+time: "Monthly. Next event Tuesday Feb. 21 4:30PM"
 color: BF360C
 ---
+<p><a href="http://www.westchester-cvc.org/talk-series.html">Redirect to Westchester-CVC Website</a></p>
