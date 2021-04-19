147fc78f98f791f20da1b08bcb940b183ff767fd
Joe Bradley <jlbradley1844@gmail.com>
2021-04-18 21:54:16 -0400

    April 18 updates

---
+++ _bahai-meeting/2021-04-23-ClusterMtg.md
@@ -0,0 +1,11 @@
+---
+layout: post
+title:  "Cluster Reflection Meeting" 
+location: Electronic (Zoom)
+time: "Friday April 23, 8:00PM-10:00PM"
+color: BF360C
+---
+The next cluster reflection meeting for Westchester county will
+take place Friday April 23 on Zoom.
+Contact <atc@westchesterbahais.org> for Zoom link to join
+the celebrations and community planning.
--- _special-events/vibrant_communities.mk
+++ _special-events/vibrant_communities.mk
@@ -1,10 +1,10 @@
 ---
 layout: post
 title:  "Interfaith Devotional & Talk Series"
-description: "Interfaith Talk Series"
+description: "Interfaith Talk Series. April Topic: A New Model of Education"
 location: "Electronic (Zoom)"
-exturl: http://www.westchester-cvc.org/talk-series.html
-time: "Monthly. March event and topic TBD. Check link for updates."
+exturl: "http://www.westchester-cvc.org/talk-series.html"
+time: "Monthly. Next event April 25 4:00PM-6:00PM."
 color: BF360C
 ---
 <p><a href="http://www.westchester-cvc.org/talk-series.html">Redirect to Westchester-CVC Website</a></p>
