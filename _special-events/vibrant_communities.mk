ada689068af5a009ef8d33fcbe9e7d749a08e083
Joe Bradley <jlbradley1844@users.noreply.github.com>
2021-06-03 21:26:41 -0400

    Update vibrant_communities.mk

--- _special-events/vibrant_communities.mk
+++ _special-events/vibrant_communities.mk
@@ -1,10 +1,10 @@
 ---
 layout: post
 title:  "Interfaith Devotional & Talk Series"
-description: "Interfaith Talk Series. April Topic: A New Model of Education"
+description: "Interfaith Talk Series. June Topic: Better Elections? Lessons from an Unusual Source"
 location: "Electronic (Zoom)"
 exturl: "http://www.westchester-cvc.org/talk-series.html"
-time: "Monthly. Next event April 25 4:00PM-6:00PM."
+time: "Monthly. Next event June 6 4:00PM-6:00PM."
 color: BF360C
 ---
 <p><a href="http://www.westchester-cvc.org/talk-series.html">Redirect to Westchester-CVC Website</a></p>
